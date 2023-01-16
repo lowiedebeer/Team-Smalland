327e1abb094a2767a6b72c7e6603629fabda2da9
KarelLaan <kareljibbevanderlaan827@gmail.com>
2023-01-16 13:20:22 +0100

    Update train.py

--- train.py
+++ train.py
@@ -2,8 +2,7 @@ class Train():
     def __init__(self, current_station, traject_length):
        
         self.train = current_station
-        self.speed = 1 # in minuten
-        self.traject_length = traject_length # in minuten
+        self.traject_length = traject_length # In minuten
         self.list_of_stations = [current_station]
         self.location=0
 
@@ -12,7 +11,7 @@ class Train():
         The movement of a single train is defined by its speed if the destination
         is reached the station will be added to a list of stations on its traject.
         """
-        self.location += self.speed
+        self.location += 1
        
        # If the distance between the train and the station is 0 add it to the list of stations
         if self.traject_length[1] - self.location == 0:
@@ -22,7 +21,3 @@ class Train():
         return self.list_of_stations
 
 
-testing = Train("Alkmaar", ("Hoorn", 24))
-for i in range(50):
-    werkend = testing.movement()
-    print(werkend)
